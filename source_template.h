6061606b356a938695727e10fc851d584ac36a4f
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2015-10-25 13:42:49 +0100

    Setup build system and basic tools

---
+++ source_template.h
@@ -0,0 +1,7 @@
+// Copyright (c) {YEAR} Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+namespace CodeHero {
+
+} // namespace CodeHero
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+
+int main() {
+  std::cout << "This is working at least" << std::endl;
+  return 0;
+}
