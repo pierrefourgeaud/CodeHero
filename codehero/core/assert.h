f5612aa7676ab28403c7652f96f5ea3c1d9335ca
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2016-09-11 20:17:16 +0200

    Add missing assert file

---
+++ codehero/core/assert.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2016 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef CODEHERO_CORE_ASSERT_H_
+#define CODEHERO_CORE_ASSERT_H_
+
+#include <cassert>
+#include <logger.h>
+
+#define CH_ASSERT(cond) \
+    if (! (cond)) { LOGE << "Assertion failed: " << #cond << std::endl; } \
+    assert(cond);
+
+#endif // CODEHERO_CORE_ASSERT_H_
