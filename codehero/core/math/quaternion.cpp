321b3b4a60671140148d481a66f5348e9261a246
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-08-06 17:28:23 +0100

    Added Quaternion class

---
+++ codehero/core/math/quaternion.cpp
@@ -0,0 +1,147 @@
+// Copyright (c) 2017 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "core/math/quaternion.h"
+#include "core/math/vector3.h"
+#include "core/math/matrix4.h"
+#include "core/math/utils.h"
+#include <math.h>
+
+namespace CodeHero {
+
+void Quaternion::FromAngleAndAxis(float iAngle, const Vector3& iAxis) {
+    Vector3 normAxis = iAxis;
+    normAxis.Normalize();
+    
+    float radians = DegToRad(iAngle);
+    float sinAngle = sinf(radians);
+    float cosAngle = cosf(radians);
+
+    m_W = cosAngle;
+    m_X = normAxis.x() * sinAngle;
+    m_Y = normAxis.y() * sinAngle;
+    m_Z = normAxis.z() * sinAngle;
+}
+
+// This function is inspired from Urho3D FromEulerAngles
+// (no need to try to come up with something different here...)
+void Quaternion::FromEulerAngles(float iX, float iY, float iZ) {
+    // Order of rotations: Z first, then X, then Y
+    // (like typical FPS camera with gimbal lock at top/bottom)
+    float x = DegToRad(iX);
+    float y = DegToRad(iY);
+    float z = DegToRad(iZ);
+    float sinX = sinf(x);
+    float cosX = cosf(x);
+    float sinY = sinf(y);
+    float cosY = cosf(y);
+    float sinZ = sinf(z);
+    float cosZ = cosf(z);
+
+    m_W = cosY * cosX * cosZ + sinY * sinX * sinZ;
+    m_X = cosY * sinX * cosZ + sinY * cosX * sinZ;
+    m_Y = sinY * cosX * cosZ - cosY * sinX * sinZ;
+    m_Z = cosY * cosX * sinZ - sinY * sinX * cosZ;
+}
+
+void Quaternion::FromRotationMatrix(const Matrix4& iMat) {
+    float fourXSquaredMinus1 = iMat.Get(0, 0) - iMat.Get(1, 1) - iMat.Get(2, 2);
+    float fourYSquaredMinus1 = iMat.Get(1, 1) - iMat.Get(0, 0) - iMat.Get(2, 2);
+    float fourZSquaredMinus1 = iMat.Get(2, 2) - iMat.Get(0, 0) - iMat.Get(1, 1);
+    float fourWSquaredMinus1 = iMat.Get(0, 0) + iMat.Get(1, 1) + iMat.Get(2, 2);
+
+    int biggestIndex = 0;
+    float fourBiggestSquaredMinus1 = fourWSquaredMinus1;
+    if (fourXSquaredMinus1 > fourBiggestSquaredMinus1) {
+        fourBiggestSquaredMinus1 = fourXSquaredMinus1;
+        biggestIndex = 1;
+    }
+    if (fourYSquaredMinus1 > fourBiggestSquaredMinus1) {
+        fourBiggestSquaredMinus1 = fourYSquaredMinus1;
+        biggestIndex = 2;
+    }
+    if (fourZSquaredMinus1 > fourBiggestSquaredMinus1) {
+        fourBiggestSquaredMinus1 = fourZSquaredMinus1;
+        biggestIndex = 3;
+    }
+
+    float biggestVal = sqrt(fourBiggestSquaredMinus1 + 1.0f) * 0.5f;
+    float mult = 0.25f / biggestVal;
+
+    switch (biggestIndex) {
+    case 0:
+        m_W = biggestVal;
+        m_X = (iMat.Get(1, 2) - iMat.Get(2, 1)) * mult;
+        m_Y = (iMat.Get(2, 0) - iMat.Get(0, 2)) * mult;
+        m_Z = (iMat.Get(0, 1) - iMat.Get(1, 0)) * mult;
+        break;
+    case 1:
+        m_W = (iMat.Get(1, 2) - iMat.Get(2, 1)) * mult;
+        m_X = biggestVal;
+        m_Y = (iMat.Get(0, 1) + iMat.Get(1, 0)) * mult;
+        m_Z = (iMat.Get(2, 0) + iMat.Get(0, 2)) * mult;
+        break;
+    case 2:
+        m_W = (iMat.Get(2, 0) - iMat.Get(0, 2)) * mult;
+        m_X = (iMat.Get(0, 1) + iMat.Get(1, 0)) * mult;
+        m_Y = biggestVal;
+        m_Z = (iMat.Get(1, 2) + iMat.Get(2, 1)) * mult;
+        break;
+    case 3:
+        m_W = (iMat.Get(0, 1) - iMat.Get(1, 0)) * mult;
+        m_X = (iMat.Get(2, 0) + iMat.Get(0, 2)) * mult;
+        m_Y = (iMat.Get(1, 2) + iMat.Get(2, 1)) * mult;
+        m_Z = biggestVal;
+        break;
+
+    default:
+        // Should never reach here - Simply for sanity
+        break;
+    }
+}
+
+void Quaternion::FromAxis(const Vector3& iXAxis, const Vector3& iYAxis, const Vector3& iZAxis) {
+    Matrix4 matrix(iXAxis.x(), iYAxis.x(), iZAxis.x(), 0.0f,
+                   iXAxis.y(), iYAxis.y(), iZAxis.y(), 0.0f,
+                   iXAxis.z(), iYAxis.z(), iZAxis.z(), 0.0f,
+                   0.0f      , 0.0f      , 0.0f      , 1.0f);
+
+    FromRotationMatrix(matrix);
+}
+
+void Quaternion::FromLookAt(const Vector3& iDirection, const Vector3& iUp) {
+    Vector3 direction = iDirection;
+    direction.Normalize();
+    Vector3 right = (direction).Cross(iUp).Normalize();
+    Vector3 up = right.Cross((direction));
+
+    Matrix4 matrix(right.x(), up.x(), -direction.x(), 0.0f,
+                   right.y(), up.y(), -direction.y(), 0.0f,
+                   right.z(), up.z(), -direction.z(), 0.0f,
+                   0.0f     , 0.0f  , 0.0f          , 1.0f);
+
+    FromRotationMatrix(matrix);
+}
+
+Matrix4 Quaternion::RotationMatrix() const {
+    Matrix4 result;
+    float xx(m_X * m_X);
+    float yy(m_Y * m_Y);
+    float zz(m_Z * m_Z);
+    float xz(m_X * m_Z);
+    float xy(m_X * m_Y);
+    float yz(m_Y * m_Z);
+    float wx(m_W * m_X);
+    float wy(m_W * m_Y);
+    float wz(m_W * m_Z);
+
+    return Matrix4(
+        1.0f - 2.0f * (yy + zz), 2.0f * (xy + wz)        , 2.0f * (xz - wy),        0.0f,
+        2.0f * (xy - wz)       , 1.0f - 2.0f * (xx + zz) , 2.0f * (yz + wx),        0.0f,
+        2.0f * (xz + wy)       , 2.0f * (yz - wx)        , 1.0f - 2.0f * (xx + yy), 0.0f,
+        0.0f                   , 0.0f                    , 0.0f                   , 1.0f
+    );
+}
+
+} // namespace CodeHero
---
+++ codehero/core/math/quaternion.h
@@ -0,0 +1,111 @@
+// Copyright (c) 2017 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef CODEHERO_CORE_MATH_QUATERNION_H_
+#define CODEHERO_CORE_MATH_QUATERNION_H_
+
+namespace CodeHero {
+
+// Forward declaration
+class Vector3;
+class Matrix4;
+
+class Quaternion {
+public:
+    Quaternion()
+        : m_W(1.0f)
+        , m_X(0.0f) 
+        , m_Y(0.0f) 
+        , m_Z(0.0f) {} // identity quaternion
+
+    Quaternion(const Quaternion& iRhs)
+        : m_W(iRhs.m_W)
+        , m_X(iRhs.m_X)
+        , m_Y(iRhs.m_Y)
+        , m_Z(iRhs.m_Z) {} // Copy ctor
+
+    Quaternion(float iW, float iX, float iY, float iZ)
+        : m_W(iW)
+        , m_X(iX)
+        , m_Y(iY)
+        , m_Z(iZ) {} // Explicit construction
+
+    Quaternion(float iAngle, const Vector3& iAxis) { FromAngleAndAxis(iAngle, iAxis); }
+    Quaternion(float iX, float iY, float iZ) { FromEulerAngles(iX, iY, iZ); }
+    explicit Quaternion(const Matrix4& iMat) { FromRotationMatrix(iMat); }
+    // From orthonormal axis
+    Quaternion(const Vector3& iXAxis, const Vector3& iYAxis, const Vector3& iZAxis) {
+        FromAxis(iXAxis, iYAxis, iZAxis);
+    }
+    // Look at
+    Quaternion(const Vector3& iDirection, const Vector3& iUp) { FromLookAt(iDirection, iUp); }
+
+    ~Quaternion() {}
+
+    // Builder
+    void FromAngleAndAxis(float iAngle, const Vector3& iAxis);
+    void FromEulerAngles(float iX, float iY, float iZ);
+    void FromRotationMatrix(const Matrix4& iMat);
+    void FromAxis(const Vector3& iXAxis, const Vector3& iYAxis, const Vector3& iZAxis);
+    void FromLookAt(const Vector3& iDirection, const Vector3& iUp);
+
+    // Operators assignment
+    Quaternion& operator=(const Quaternion& iRhs) {
+        m_W = iRhs.m_W;
+        m_X = iRhs.m_X;
+        m_Y = iRhs.m_Y;
+        m_Z = iRhs.m_Z;
+
+        return *this;
+    }
+
+    Quaternion& operator+=(const Quaternion& iRhs) {
+        m_W += iRhs.m_W;
+        m_X += iRhs.m_X;
+        m_Y += iRhs.m_Y;
+        m_Z += iRhs.m_Z;
+
+        return *this;
+    }
+
+    Quaternion& operator*=(const Quaternion& iRhs) {
+        m_W *= iRhs.m_W;
+        m_X *= iRhs.m_X;
+        m_Y *= iRhs.m_Y;
+        m_Z *= iRhs.m_Z;
+
+        return *this;
+    }
+
+    // Operators comparison
+    bool operator==(const Quaternion& iRhs) { // TODO(pierre) We should probably use the utils FloatEqu here
+        return m_X == iRhs.m_X && m_Y == iRhs.m_Y && m_Z == iRhs.m_Z && m_W == iRhs.m_W;
+    }
+
+    bool operator!=(const Quaternion& iRhs) {
+        return !(*this == iRhs);
+    }
+
+    // Accessors
+    float w() const { return m_W; }
+    float x() const { return m_X; }
+    float y() const { return m_Y; }
+    float z() const { return m_Z; }
+    const float* Data() const { return &m_W; }
+
+    Matrix4 RotationMatrix() const;
+
+private:
+
+    // Quaternion data - Should stay at the end of the class declaration
+    //                   because of the Data() accessor
+    float m_W;
+    float m_X;
+    float m_Y;
+    float m_Z;
+};
+
+} // namespace CodeHero
+
+#endif // CODEHERO_CORE_MATH_QUATERNION_H_
