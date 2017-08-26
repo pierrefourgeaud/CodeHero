0b26a8948131fc11e4ab4a4670d0e34989a70afb
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-08-26 12:32:44 +0200

    Added scopecleaner utility to help avoiding leaks

---
+++ codehero/core/scopecleaner.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2017 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef CODEHERO_CORE_SCOPECLEANER_H_
+#define CODEHERO_CORE_SCOPECLEANER_H_
+
+#include <functional>
+
+namespace CodeHero {
+
+template <class T>
+class ScopeCleaner {
+public:
+    ScopeCleaner(T&& iAction) : m_Action(std::move(iAction)) {}
+    ~ScopeCleaner() {
+        m_Action();
+    }
+
+private:
+    T m_Action;
+};
+
+// Needed to have simple template deduction
+template <class T>
+ScopeCleaner<T> Cleanup(T&& iAction) {
+    return ScopeCleaner<T>(std::forward<T>(iAction));
+}
+
+} // namespace CodeHero
+
+#endif // CODEHERO_CORE_SCOPECLEANER_H_
