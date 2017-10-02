c90ebdef418734e5734dc038ab8e7283113bc630
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-10-02 20:35:20 +0200

    First attempt at castable attribute accessor

--- codehero/core/objectdefinition.h
+++ codehero/core/objectdefinition.h
@@ -76,6 +76,61 @@ private:
     SetFn m_pSet;
 };
 
+// This is a special class. This requires that:
+//   - Type can be casted into CastTo (Usually instance of CastTo that is a child of Type)
+//   - Also requires that CastTo can be cast statically to Type (again inheritance usually)
+// TODO(pierre) This is error prone - We should try to find a better way to achieve this
+template <class Class, class Type, class CastTo, class GetFn, class SetFn>
+class AttributeAccessorImplCast : public AttributeAccessor {
+public:
+    AttributeAccessorImplCast(GetFn iGet, SetFn iSet)
+        : m_pGet(iGet)
+        , m_pSet(iSet) {}
+
+    Variant Get(const Serializable* iPtr) const override {
+        CH_ASSERT(iPtr);
+        const Class* classPtr = static_cast<const Class*>(iPtr);
+        return Variant((classPtr->*m_pGet)());
+    }
+
+    void Set(Serializable* iPtr, const Variant& iValue) override {
+        CH_ASSERT(iPtr);
+        // TODO(pierre) It would be nice to make it safe
+        //   One way would be to use the information that we have on our type
+        //   i.e: "Shader" IsTypeOf "Serializable"
+        Class* classPtr = static_cast<Class*>(iPtr);
+        (classPtr->*m_pSet)(reinterpret_cast<CastTo>(iValue.Get<Type>()));
+    }
+
+private:
+    GetFn m_pGet;
+    SetFn m_pSet;
+};
+
+template <class Class, class Type, class CastTo, class SetFn>
+class AttributeAccessorImplCast<Class, Type, CastTo, std::nullptr_t, SetFn> : public AttributeAccessor {
+public:
+    AttributeAccessorImplCast(void*, SetFn iSet)
+        : m_pSet(iSet) {}
+
+    Variant Get(const Serializable* iPtr) const override {
+        CH_ASSERT(iPtr);
+        return Variant();
+    }
+
+    void Set(Serializable* iPtr, const Variant& iValue) override {
+        CH_ASSERT(iPtr);
+        // TODO(pierre) It would be nice to make it safe
+        //   One way would be to use the information that we have on our type
+        //   i.e: "Shader" IsTypeOf "Serializable"
+        Class* classPtr = static_cast<Class*>(iPtr);
+        (classPtr->*m_pSet)(reinterpret_cast<CastTo>(iValue.Get<Type>()));
+    }
+
+private:
+    SetFn m_pSet;
+};
+
 template <class Class, class Type, class GetFn, class SetFn>
 std::shared_ptr<AttributeAccessorImpl<Class, Type, GetFn, SetFn>> MakeAccessorImpl(GetFn iGet, SetFn iSet) {
     return std::make_shared<AttributeAccessorImpl<Class, Type, GetFn, SetFn>>(iGet, iSet);
