5fbe5acdd0ca61e932e315f6dbb09494cd530efa
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-10-07 12:11:03 +0200

    Cleaned repo by removing unused python scripts

--- source_template.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) {YEAR} Pierre Fourgeaud
-// Use of this source code is governed by the MIT license that can be
-// found in the LICENSE file.
-
-namespace CodeHero {
-
-}  // namespace CodeHero
