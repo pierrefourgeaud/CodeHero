a19a658b72def46ab2dee82193398df51181dcca
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-10-22 13:51:07 +0200

    Added Color as an utility object

---
+++ codehero/core/color.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2017 Pierre Fourgeaud
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef CODEHERO_CORE_COLOR_H_
+#define CODEHERO_CORE_COLOR_H_
+
+namespace CodeHero {
+
+class Color {
+public:
+    Color();
+    Color(float iR, float iG, float iB, float iA = 0.0f);
+    ~Color();
+
+    float r() const { return m_Rgba[0]; }
+    float g() const { return m_Rgba[1]; }
+    float b() const { return m_Rgba[2]; }
+    float a() const { return m_Rgba[3]; }
+
+    static const Color Black;
+    static const Color White;
+    static const Color Red;
+    static const Color Green;
+    static const Color Blue;
+
+private:
+    float m_Rgba[4] = { 0 };
+};
+
+} // namespace CodeHero
+
+#endif // CODEHERO_CORE_COLOR_H_
