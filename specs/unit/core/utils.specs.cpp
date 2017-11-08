d4a068ef50f681b6e3b082bd235f5deb2c8fc4a4
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2017-11-08 08:53:03 +0100

    Added more cases for StartsWith/EndsWith tests

--- specs/unit/core/utils.specs.cpp
+++ specs/unit/core/utils.specs.cpp
@@ -13,6 +13,7 @@ go_bandit([]() {
     describe("::StartsWith", [] {
         it("should return true if starts we correct prefix", [] {
             AssertThat(StartsWith("This is a test", "This"), IsTrue());
+            AssertThat(StartsWith("This is a test", "This is a test"), IsTrue());
         });
 
         it("should return true if the prefix is empty", [] {
@@ -21,12 +22,15 @@ go_bandit([]() {
 
         it("should return false if the prefix doesn't match the start of the string", [] {
             AssertThat(StartsWith("This is a test", "Wrong"), IsFalse());
+            AssertThat(StartsWith("This is a test", "This iz"), IsFalse());
+            AssertThat(StartsWith("This is a test", "Zhis is"), IsFalse());
         });
     });
 
     describe("::EndsWith", [] {
         it("should return true if ends with correct suffix", [] {
             AssertThat(EndsWith("This is a test", "test"), IsTrue());
+            AssertThat(EndsWith("This is a test", "This is a test"), IsTrue());
         });
 
         it("should return true if suffix is empty", [] {
@@ -35,6 +39,8 @@ go_bandit([]() {
 
         it("should return false if the suffix doesn't match the end of the string", [] {
             AssertThat(EndsWith("This is a test", "wrong"), IsFalse());
+            AssertThat(EndsWith("This is a test", "tesz"), IsFalse());
+            AssertThat(EndsWith("This is a test", "zest"), IsFalse());
         });
     });
 });
